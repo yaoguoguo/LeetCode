953260f63d62fbf5f3090786a604f09b8b4725b0
yaoqihao <yaoqihao@bytedance.com>
2019-11-25 18:35:33 +0800

    start
    Change-Id: I9e750552557b21a50a84e76e25c2cd955ef870fd

---
+++ LeetCode/LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by 姚启灏 on 2019/11/25.
+//  Copyright © 2019 姚启灏. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
