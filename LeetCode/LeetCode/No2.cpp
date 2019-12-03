2449c2d0a05833a36eab1817e99c5846477b0f17
yaoqihao <yaoqihao@bytedance.com>
2019-12-03 19:11:04 +0800

    2019.12.3-No2
    Change-Id: I438facd481631515f1c654f85c337d6cb1e7b529

---
+++ LeetCode/LeetCode/No2.cpp
@@ -0,0 +1,41 @@
+//
+//  No2.cpp
+//  LeetCode
+//
+//  Created by 姚启灏 on 2019/12/3.
+//  Copyright © 2019 姚启灏. All rights reserved.
+//
+
+#include <stdio.h>
+#include "vector"
+#include <map>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    int sum=0;
+    ListNode *l3=NULL;
+    ListNode **node=&l3;
+    while(l1!=NULL||l2!=NULL||sum>0)
+    {
+        if(l1!=NULL)
+        {
+            sum+=l1->val;
+            l1=l1->next;
+        }
+        if(l2!=NULL)
+        {
+            sum+=l2->val;
+            l2=l2->next;
+        }
+        (*node)=new ListNode(sum%10);
+        sum/=10;
+        node=&((*node)->next);
+    }
+    return l3;
+}
